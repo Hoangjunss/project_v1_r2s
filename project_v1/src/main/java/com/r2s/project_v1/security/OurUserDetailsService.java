8de9db6e8d0a215acccba5d17363a6fde0dcd0c4
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-09-16 13:39:06 +0700

    success

---
+++ project_v1/src/main/java/com/r2s/project_v1/security/OurUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.r2s.project_v1.security;
+
+
+import com.r2s.project_v1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OurUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                             .orElseThrow(()-> new UsernameNotFoundException("User not found with email: " + username));
+    }
+}
---
+++ project_v1/src/main/java/com/r2s/project_v1/services/user/UserService.java
@@ -0,0 +1,4 @@
+package com.r2s.project_v1.services.user;
+
+public interface UserService {
+}
