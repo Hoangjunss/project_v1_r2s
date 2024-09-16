f2cb150272c0faa78fef0b4ac2adaf603faac573
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-09-16 13:12:28 +0700

    add

---
+++ project_v1/src/test/java/com/r2s/project_v1/ProjectV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r2s.project_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
